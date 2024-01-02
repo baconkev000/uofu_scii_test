1119a33aa3d73e35bc651c868b1ba0bfd9150a75
baconkev000 <kevinbaconweb@gmail.com>
2024-01-02 12:49:25 -0700

    add api service

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+export default instance;
+
+export class APIService {
+  /**
+   * First step login for Token Auth
+   * @param email
+   * @param password
+   * @returns Promise
+   */
+  static login = async (email: string, password: string) =>
+    instance.post(`auth-token`, { email: email, password: password });
+
+  /**
+   * Get Players
+   * @returns Promise
+   */
+  static getPlayers = async () => instance.get("api/players/");
+}
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="home">
     <div class="text-3xl">Hello</div>
+    <li v-for="player in players" :key="player.pk" class="text-black text-2xl">
+      {{ player.fields.Name }}
+    </li>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-@Options({
-  components: {},
-})
-export default class HomeView extends Vue {}
+import { Vue } from "vue-class-component";
+import { APIService } from "@/services/api";
+import type { Player } from "@/types/player";
+import { ref } from "vue";
+
+export default class HomeView extends Vue {
+  players = ref();
+  info = ref();
+
+  loading = ref(true);
+
+  data() {
+    return {
+      players: undefined,
+    };
+  }
+
+  mounted() {
+    APIService.getPlayers()
+      .then((response: any) => (this.players = response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
 </script>
