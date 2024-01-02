9391a62fa74192b6fbc27067de0622884954bc4f
baconkev000 <kevinbaconweb@gmail.com>
2024-01-02 16:56:37 -0700

    fix cors issue and update frontend api table

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="w-full flex justify-center items-center">
+  <div class="w-full flex justify-center items-center flex-col">
+    <div class="flex flex-row justify-end w-5/6 py-8">
+      <button
+        id="dropdownCheckboxButton"
+        data-dropdown-toggle="dropdownDefaultCheckbox"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+      >
+        Dropdown checkbox
+        <svg
+          class="w-2.5 h-2.5 ms-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+
+      <!-- Dropdown menu -->
+      <div
+        id="dropdownDefaultCheckbox"
+        class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+      >
+        <ul
+          class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownCheckboxButton"
+        >
+          <li>
+            <div class="flex items-center">
+              <input
+                id="checkbox-item-1"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="checkbox-item-1"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Default checkbox</label
+              >
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <input
+                checked
+                id="checkbox-item-2"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="checkbox-item-2"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Checked state</label
+              >
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <input
+                id="checkbox-item-3"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="checkbox-item-3"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >Default checkbox</label
+              >
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
     <div class="relative overflow-x-auto shadow-md">
       <table class="w-full text-sm text-left rtl:text-right text-black">
         <thead class="text-xs text-gray-700 uppercase bg-gray-600 text-white">
@@ -9,6 +93,7 @@
             </th>
           </tr>
         </thead>
+        <div v-if="loading">Loading...</div>
         <tbody>
           <tr
             class="bg-white border-b bg-slate-200 even:bg-gray-100 odd:bg-gray-200 hover:bg-white cursor-pointer"
@@ -19,43 +104,31 @@
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap font-bold"
             >
-              {{ player.fields.Name }}
+              {{ player.Name }}
             </th>
             <td class="px-6 py-4">
-              {{
-                player.fields.Nationality ? player.fields.Nationality : "N/A"
-              }}
+              {{ player.Nationality ? player.Nationality : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{
-                player.fields.National_Position
-                  ? player.fields.Nationality
-                  : "N/A"
-              }}
+              {{ player.National_Position ? player.Nationality : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{ player.fields.Club ? player.fields.Club : "N/A" }}
+              {{ player.Club ? player.Club : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{ player.fields.Height ? player.fields.Height : "N/A" }}
+              {{ player.Height ? player.Height : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{
-                player.fields.Preffered_Foot
-                  ? player.fields.Preffered_Foot
-                  : "N/A"
-              }}
+              {{ player.Preffered_Foot ? player.Preffered_Foot : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{
-                player.fields.Ball_Control ? player.fields.Ball_Control : "N/A"
-              }}
+              {{ player.Ball_Control ? player.Ball_Control : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{ player.fields.Dribbling ? player.fields.Dribbling : "N/A" }}
+              {{ player.Dribbling ? player.Dribbling : "N/A" }}
             </td>
             <td class="px-6 py-4">
-              {{ player.fields.Vision ? player.fields.Vision : "N/A" }}
+              {{ player.Vision ? player.Vision : "N/A" }}
             </td>
           </tr>
         </tbody>
@@ -69,10 +142,10 @@ import { Vue } from "vue-class-component";
 import { APIService } from "@/services/api";
 import type { Player } from "@/types/player";
 import { ref } from "vue";
-import fixture from "@/fixture.json";
+import type { Ref } from "vue";
 
 export default class HomeView extends Vue {
-  t_rows = ref([
+  t_rows: Ref<string[]> = ref([
     "Name",
     "Nationality",
     "National Position",
@@ -82,22 +155,16 @@ export default class HomeView extends Vue {
     "Ball Control",
     "Dribbling",
     "Vision",
-  ] as string[]);
-  info = ref();
+  ]);
   loading = ref(true);
+  players: Ref<Player[]> = ref([]);
 
-  data() {
-    return {
-      players: fixture,
-    };
+  async mounted() {
+    console.log(this.players);
+    APIService.getPlayers()
+      .then((response) => (this.players = response.data))
+      .catch((error) => console.log(error))
+      .finally((this.loading = false));
   }
-
-  // mounted() {
-  //   APIService.getPlayers()
-  //     .then((response: any) => (this.players = response.data))
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
 }
 </script>
